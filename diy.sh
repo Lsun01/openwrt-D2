49c2a2d15930351f0321cbdd5d8dc31dd197450c
root <xroot009@icloud.com>
2020-02-08 20:12:46 +0800

    update yml

--- diy.sh
+++ diy.sh
@@ -6,4 +6,8 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+./scripts/feeds update -a
+./scripts/feeds install -a
