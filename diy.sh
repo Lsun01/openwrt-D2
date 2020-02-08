a43a794b3d8094e3e735b1582cd8923838a00c53
Lsun01 <xroot009@icloud.com>
2020-02-08 22:52:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,6 +10,6 @@ sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_genera
 git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/Lienol/openwrt-package package/openwrt-package
-cd openwrt
+#now dir is openwrt
 ./scripts/feeds update -a
 ./scripts/feeds install -a
